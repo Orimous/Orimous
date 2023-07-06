43fb249078177e7bd008ceca38c243906cc81471
Orimous <orimous@gmail.com>
2023-07-06 11:49:37 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const sortBtns = document.querySelectorAll('.sort-btn');
+const projectList = document.querySelector('.project-list');
+
+sortBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const filter = btn.getAttribute('data-filter');
+
+        // Удаляем активный класс у всех кнопок
+        sortBtns.forEach(btn => btn.classList.remove('active'));
+        // Добавляем активный класс только к выбранной кнопке
+        btn.classList.add('active');
+
+        // Проходимся по всем проектам и прячем/показываем их в зависимости от выбранного фильтра
+        for (let project of projectList.children) {
+            if (filter === 'all') {
+                project.style.display = 'block';
+            } else if (project.classList.contains(filter)) {
+                project.style.display = 'block';
+            } else {
+                project.style.display = 'none';
+            }
+        }
+    });
+});
---
+++ php/server.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_POST['email'])) {
+    // Здесь указываете ваш адрес электронной почты, на который будут отправляться сообщения
+    $to = "orimouswork@gmail.com";
+    $subject = "Новое сообщение с формы обратной связи";
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $text = $_POST['text'];
+
+    $message = "Имя: ".$name."\n\nEmail: ".$email."\n\nСообщение: ".$text;
+
+    $headers = "From: ".$name." <".$email.">"."\r\n".
+        "Reply-To: ".$email."\r\n".
+        "X-Mailer: PHP/".phpversion();
+
+    if(mail($to, $subject, $message, $headers)) {
+        echo "Ваше сообщение успешно отправлено.";
+    } else {
+        echo "Произошла ошибка при отправке сообщения.";
+    }
+}
+?>
