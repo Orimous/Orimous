250d669ce9cc7dff58cf3275016bd97536ae3762
Orimous <orimous@gmail.com>
2023-07-07 21:46:11 +0300

    adaptive for mobile devices

--- js/navigation.js
+++ js/navigation.js
@@ -7,15 +7,6 @@ const navSlide = () => {
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
 
-        // Анимация для ссылок в навигационном меню
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
-
         // Анимация бургерной иконки
         burger.classList.toggle('toggle');
     });
