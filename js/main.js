320fa8418c57fe2e3ab12943786c6ed30ba1b425
Orimous <orimous@gmail.com>
2023-10-29 15:05:25 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,23 +1,52 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.menu');
-    const navLinks = document.querySelectorAll('.menu li');
+const menu = document.querySelector('.burger__menu__body')
+const menuBtn = document.querySelector('.burger__menu__icon')
 
-    // Переключение навигационного меню
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
+const body = document.body;
 
-        // Анимация бургерной иконки
-        burger.classList.toggle('toggle');
-    });
-};
+if (menu && menuBtn) {
+    menuBtn.addEventListener('click', e => {
+        menu.classList.toggle('active')
+        menuBtn.classList.toggle('active')
+        body.classList.toggle('lock')
+    })
 
-navSlide();
+    menu.addEventListener('click', e => {
+        if (e.target.classList.contains('menu__body')) {
+            menu.classList.remove('active')
+            menuBtn.classList.remove('active')
+            body.classList.remove('lock')
+        }
+    })
 
-let typed = new Typed(".multipleText", {
-    strings: ["Веб-разработчик", "UX/UI дизайнер", "Android разработчик", "Студент-информатик", "Технический энтузиаст"],
+    menu.querySelectorAll('.burger__menu__link').forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('active')
+            menuBtn.classList.remove('active')
+            body.classList.remove('lock')
+        })
+    })
+}
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach(anchor => {
+    anchor.addEventListener('click', event => {
+        event.preventDefault();
+
+        const blockID = anchor.getAttribute('href').substring(1);
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+})
+
+
+var typed = new Typed(".multipleText", {
+    strings: ["Frontend Developer", "Android Developer", "Designer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
-});
+})
